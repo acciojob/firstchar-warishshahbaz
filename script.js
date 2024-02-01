3cb6f15eb0c036dd232abe161082ba88886e1da6
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-01 13:22:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstChar(text) {
   // your code here
+	return text === "" ? "" : text.trim().charAt(0)
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+// const text = prompt("Enter text:");
+// alert(firstChar(text));
